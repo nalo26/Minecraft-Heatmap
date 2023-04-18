1057cd84bed2f9b8bf0d299cabfe93a8ed2fcee3
nalo_ <nath.v26@gmail.com>
2023-04-18 11:44:14 +0200

    Added user filter

--- README.md
+++ README.md
@@ -17,10 +17,11 @@ Options:
     -c, --center COORD      Coordinate of the center of the heatmap. Use "--center='x,z'" for negative values. [0,0]
     -s, --size SIZE         Map radius. /!\ Generated image will be 2x this size. [2000]
     -w, --world WORLD       The world to make the map from. Defaults choices are {[overworld], the_nether, the_end}
+    -u, --players PLAYERS   The players to track, splited by a comma (","). [everyone]
     -p, --palette FILE      Heatmap palette. Should be a filename in the 'palettes' folder. [rgb]
 
 Example:
-    python3 heatmap.py --center="-400,600" -s 200 -w the_nether -p nether
+    python3 heatmap.py --center="-400,600" -s 200 -w the_nether -u Notch,Jeb -p nether
 ```
 
 ## Custom palettes
@@ -30,6 +31,8 @@ For a better result, the image should have a **width of at least 256px**.
 You can generate gradients image easily using a [website maker](https://angrytools.com/gradient/image/), or any tool that you want.
 
 ## Result examples
+
+> Those examples are taken from the Survival Multiplayer (SMP) between Twitch Subscribers of [MathoX](https://twitch.tv/MathoX)
 
 `python3 heatmap.py` (equivalent to `python3 heatmap.py -c 0,0 -s 2000 -w overworld -p rgb`)  
 <img src="results/heatmap_0,0_x2000_overworld_rgb.png">
--- heatmap.py
+++ heatmap.py
@@ -2,8 +2,8 @@ import argparse
 import math
 import os
 
-from dotenv import load_dotenv
 import mysql.connector
+from dotenv import load_dotenv
 from PIL import Image
 from tqdm import tqdm
 
@@ -12,24 +12,33 @@ if os.path.exists(".env.dev"):
     load_dotenv(".env.dev", override=True)
 
 
-def tuple_type(strings: str):
+def coordinate_type(strings: str):
     strings = strings.replace("(", "").replace(")", "")
     int_tuple = tuple(map(int, strings.split(",")))
     assert len(int_tuple) == 2, "Invalid tuple length."
     return int_tuple
 
 
-def query_database(cursor, center: tuple[int], size: int, world: str):
+def players_type(strings: str):
+    return [p.strip() for p in strings.split(",")]
+
+
+def query_database(
+    cursor, center: tuple[int], size: int, world: str, players: list[str]
+):
     coordinates = {}
     query = "SELECT x, z FROM actions WHERE action_id IN (1, 2, 3)"
     query += " AND source = 5 AND world_id ="
     query += " (SELECT id FROM worlds WHERE identifier = 'minecraft:%s')"
+    if players:
+        query += " AND player_id IN (SELECT id FROM players WHERE player_name IN (%s))"
     query += " AND x BETWEEN %d AND %d AND z BETWEEN %d AND %d"
 
     cursor.execute(
         query
         % (
             world,
+            *([", ".join(f"'{p}'" for p in players)] if players else []),
             center[0] - size,
             center[0] + size,
             center[1] - size,
@@ -73,7 +82,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-c",
         "--center",
-        type=tuple_type,
+        type=coordinate_type,
         default="0,0",
         help="Coordinate of the center of the heatmap.",
     )
@@ -92,12 +101,24 @@ if __name__ == "__main__":
         help="The world to make the map from.",
     )
     parser.add_argument(
+        "-u",
+        "--players",
+        type=players_type,
+        help="Player(s) to include in the heatmap.",
+    )
+    parser.add_argument(
         "-p",
         "--palette",
         type=str,
         choices=[f.split(".")[0] for f in os.listdir("palettes")],
         default="rgb",
         help="Heatmap palette. Should be a filename in the 'palettes' folder.",
+    )
+    parser.add_argument(
+        "-o",
+        "--name",
+        type=str,
+        help="Name of the output file. Defaults to 'heatmap_<center>_x<size>_<world>_<palette>'.",
     )
     args = parser.parse_args()
 
@@ -112,14 +133,18 @@ if __name__ == "__main__":
     cursor = connection.cursor()
 
     print("Querying database...")
-    data = query_database(cursor, args.center, args.size, args.world)
+    data = query_database(cursor, args.center, args.size, args.world, args.players)
 
-    print("Generating image...")
-    image = generate_image(data, args.center, args.size, args.palette)
-    image.save(
-        f"results/heatmap_{','.join(str(n) for n in args.center)}"
-        f"_x{args.size}_{args.world}_{args.palette}.png"
-    )
+    if data:
+        print("Generating image...")
+        image = generate_image(data, args.center, args.size, args.palette)
+        name = (
+            args.name
+            or f"heatmap_{','.join(str(n) for n in args.center)}_x{args.size}_{args.world}"
+        )
+        image.save(f"results/{name}.png")
+    else:
+        print("No data found with those parameters.")
 
     cursor.close()
     connection.close()
